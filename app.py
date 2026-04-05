bdbe57a424b0820edd112b0d8034655a9f3a61e5
jdcarter17 <jdcarter17@hotmail.com>
2026-04-05 06:43:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask
 
-@app.route("/api/test")
-def test():
-    return {"participants": 120, "male": 70, "female": 50}
-
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Race Analytics Dashboard Running"
+
+@app.route("/api/test")
+def test():
+    return {"participants": 120, "male": 70, "female": 50}
 
 if __name__ == "__main__":
     app.run(debug=True)
